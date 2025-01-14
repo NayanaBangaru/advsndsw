e27c25222d06b6324ab5d31e91c1bf9f3a933b4a
Nayana Bangaru <nbangaru@cern.ch>
2025-01-14 14:16:05 +0100

    added fed algorithms

--- shipLHC/FrontendDriver.cxx
+++ shipLHC/FrontendDriver.cxx
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
 
@@ -81,27 +83,72 @@ void FrontendDriver::ZeroSuppressionAlgorithms(AdvSignal Signal)
                     temp_ClusterAmplitudes.push_back(Amplitude[i]); 
                 }
             } 
-            if (temp_ClusterStrips.size() == 1) 
+            if (Strips.size() > temp_ClusterStrips.size() && temp_ClusterStrips.size()!=0 ) 
             {
-                if (temp_ClusterStrips[0] > 5*stripsensor::frontend::StripNoise){ClusterStrips = temp_ClusterStrips; ClusterAmplitudes = temp_ClusterAmplitudes;}
-                else {ClusterStrips = temp_ClusterStrips; ClusterAmplitudes.push_back(0);}
+                if(std::find(Strips.begin(), Strips.end(), temp_ClusterStrips[0]+1)!=Strips.end()){
+                auto it = std::find(Strips.begin(), Strips.end(), temp_ClusterStrips[0]+1);
+                    if ((Amplitude[it - Strips.begin()])>0)
+                    {
+                        temp_ClusterStrips.push_back(Strips[it - Strips.begin()]);
+                        temp_ClusterAmplitudes.push_back(Amplitude[it - Strips.begin()]);
+                    } else {
+                        temp_ClusterAmplitudes.push_back(0);
+                    }
+                }
+                if(std::find(Strips.begin(), Strips.end(), temp_ClusterStrips[0]-1)!=Strips.end()){
+                auto it = std::find(Strips.begin(), Strips.end(), temp_ClusterStrips[0]-1);
+                    if ((Amplitude[it - Strips.begin()])>0)
+                    {
+                        temp_ClusterStrips.emplace(temp_ClusterStrips.begin(), Strips[it - Strips.begin()]);
+                        temp_ClusterAmplitudes.emplace(temp_ClusterAmplitudes.begin(), Amplitude[it - Strips.begin()]);
+                    } else {
+                        temp_ClusterAmplitudes.emplace(temp_ClusterAmplitudes.begin(), 0);
+                    }
+                }
             }
-            else {ClusterStrips = temp_ClusterStrips; ClusterAmplitudes = temp_ClusterAmplitudes;}
-            for (int i = 0; i < ClusterAmplitudes.size(); i++)
+            ClusterStrips = temp_ClusterStrips; 
+            ClusterAmplitudes = temp_ClusterAmplitudes; 
+            for(int k = 0; k < ClusterStrips.size(); k++)
             {
-                cout << ClusterStrips[i] << "\t" << ClusterAmplitudes[i] << endl; 
-            }  
+                cout << ClusterStrips[k] << "\t" << ClusterAmplitudes[k] << endl;
+            }
             break; 
         case 4:
-            cout << "Case 4" << endl; 
+            Double_t SumCharge = 0;
+            Double_t SumNoise = 0;
+            for (int i = 0; i < Amplitude.size(); i++)
+            {
+                if (Amplitude[i] > 3*stripsensor::frontend::StripNoise)
+                {
+                    temp_ClusterStrips.push_back(Strips[i]); 
+                    temp_ClusterAmplitudes.push_back(Amplitude[i]); 
+                    SumCharge += Amplitude[i];
+                    SumNoise += pow((stripsensor::frontend::StripNoise), 2);
+                } else if (Amplitude[i] > 2*stripsensor::frontend::StripNoise) {
+                    SumCharge += Amplitude[i];
+                    SumNoise += pow((stripsensor::frontend::StripNoise), 2);
+                    cout << SumCharge << "\t" << SumNoise << endl; 
+                    if (SumCharge > 5*SumNoise)
+                    {
+                        temp_ClusterStrips.push_back(Strips[i]); 
+                        temp_ClusterAmplitudes.push_back(Amplitude[i]);
+                    }
+                }
+            } 
+            ClusterStrips = temp_ClusterStrips; 
+            ClusterAmplitudes = temp_ClusterAmplitudes;
+            for (int k = 0; k < ClusterStrips.size(); k++)
+            {
+                cout << ClusterStrips[k] << "\t" << ClusterAmplitudes[k] << endl; 
+            }
             break; 
     }
 }
 
 void TestingAlgorithm()
 {
-    std::vector<Int_t> Strips = {100, 101}; 
-    std::vector<Double_t> IntegratedSignal = {50, 60};
+    std::vector<Int_t> Strips = {100, 101, 102}; 
+    std::vector<Double_t> IntegratedSignal = {200, 600, 50};
     
     AdvSignal TestSignal(Strips, IntegratedSignal); 
 
